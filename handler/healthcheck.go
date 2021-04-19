37c786e8516568df8380c9845b3f761b6e186f10
paulobernardo <paulo.bernardo@ilhasoft.com.br>
2021-04-19 12:18:05 -0300

    feat: check aws services

--- handler/healthcheck.go
+++ handler/healthcheck.go
@@ -1,10 +1,24 @@
 package handler
 
 import (
+	"errors"
 	"net/http"
+
+	"github.com/ilhasoft/wwcs/pkg/websocket"
+)
+
+var (
+	ErrorAWSConnection = errors.New("Unable to connect to AWS")
 )
 
 // HealthCheckHandler is used to provide a mechanism to check the service status
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	err := websocket.CheckAWS()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(ErrorAWSConnection.Error()))
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
 }
